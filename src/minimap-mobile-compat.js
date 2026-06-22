5557f68e9d65a6a351238098efc5721328fae823
cj53189 <130353602+cj53189@users.noreply.github.com>
2026-06-21 19:32:41 -0500

    Load mobile haptics compatibility module

--- src/minimap-mobile-compat.js
+++ src/minimap-mobile-compat.js
@@ -1,1 +1,10 @@
 drawDynamicMarkers = true;
+
+(function loadMobileHapticsModule() {
+  if (document.querySelector('script[src="./src/haptics.js"]')) return;
+  const script = document.createElement("script");
+  script.src = "./src/haptics.js";
+  script.async = false;
+  script.defer = true;
+  document.head.appendChild(script);
+})();
