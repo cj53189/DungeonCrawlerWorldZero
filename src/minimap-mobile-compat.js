db479fa72a781741791854bfbaaad096214276f1
cj53189 <130353602+cj53189@users.noreply.github.com>
2026-06-21 19:53:57 -0500

    Load spawn allocation UI module

--- src/minimap-mobile-compat.js
+++ src/minimap-mobile-compat.js
@@ -8,3 +8,12 @@ drawDynamicMarkers = true;
   script.defer = true;
   document.head.appendChild(script);
 })();
+
+(function loadSpawnAllocationUiModule() {
+  if (document.querySelector('script[src="./src/spawn-allocation-ui.js"]')) return;
+  const script = document.createElement("script");
+  script.src = "./src/spawn-allocation-ui.js";
+  script.async = false;
+  script.defer = true;
+  document.head.appendChild(script);
+})();
